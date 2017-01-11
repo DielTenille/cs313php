53c886e6c86731bf662f777918c3d9a13dc68b5d
Tenille <die12010@byui.edu>
2017-01-10 23:49:32 -0700

    Another try at JavaScript

--- web/team-activity/javascript.js
+++ web/team-activity/javascript.js
@@ -5,6 +5,12 @@ function btnClicked() {
 
 function updateColor1() {
     var colorRequested = document.getElementById("changeColor1").value;
-    var div1 = document.getElementById("firstDiv")
+    var div1 = document.getElementById("firstDiv");
     div1.style.backgroundColor = colorRequested;
+}
+function updateColor2() {
+    var inputID = document.getElementById("changeColor");
+    var colorRequested = inputID.value;
+    var div2 = document.getElementById("secondDiv");
+    div2.style.backgroundColor = colorRequested;
 }
